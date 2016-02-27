ec4f12ff40d8f6500938e994a9c69e2084dcbff6
Alexander Wong <admin@alexander-wong.com>
2016-02-26 18:14:17 -0700

    Initialized basic websocket files

---
+++ app.js
@@ -0,0 +1,90 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('node-sass-middleware')({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: true,
+  sourceMap: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+var WebSocketServer = require('ws').Server, wss = new WebSocketServer({port: 8080});
+
+console.log('Websocket server started on 8080');
+
+var rabbit = {x:0, y:0};
+
+wss.on('connection', function(ws) {
+  ws.on('message', function(message) {
+    var incommingMsg = JSON.parse(message);
+    rabbit.x = incommingMsg.x;
+    rabbit.y = incommingMsg.y;
+    for(var i in wss.clients) {
+      wss.clients[i].send(JSON.stringify(rabbit));
+    }
+
+  });
+  ws.send(JSON.stringify(rabbit));
+});
+
+/*
+app.listen(3000, function () {
+  console.log('Web server started on 3000!');
+});
+*/
+
+module.exports = app;
---
+++ public/javascripts/game.js
@@ -0,0 +1,52 @@
+var game = new Phaser.Game(800, 500, Phaser.AUTO, 'phaser', { preload: preload, create: create});
+var myText = null;
+var sprite = null;
+
+function preload() {
+  game.load.image("rabbit", "images/rabbit.png");
+}
+
+function create() {
+  this.client = new Client();
+  this.client.openConnection();
+  myText = game.add.text(0, 0, "started (not yet connected)", { font: "14px Arial", fill: "#ff0044"});
+  sprite = game.add.sprite(100, 100, "rabbit");
+  sprite.inputEnabled = true;
+  sprite.input.enableDrag(false, true);
+  sprite.events.onDragStop.add(rabbitDragged, this);
+  game.stage.disableVisibilityChange = true;
+}
+
+function rabbitDragged() {
+  if (this.client.connected) {
+    this.client.ws.send(JSON.stringify({x: sprite.x, y: sprite.y}));
+  }
+}
+
+function Client() {
+
+}
+
+Client.prototype.openConnection = function() {
+  this.ws = new WebSocket("ws://127.0.0.1:8080");
+  this.connected = false;
+  this.ws.onmessage = this.onMessage.bind(this);
+  this.ws.onerror = this.displayError.bind(this);
+  this.ws.onopen = this.connectionOpen.bind(this);
+};
+
+Client.prototype.connectionOpen = function() {
+  this.connected = true;
+  myText.text = 'connected\n';
+};
+
+Client.prototype.onMessage = function(message) {
+  myText.text = myText.text + message.data;
+  var msg = JSON.parse(message.data);
+  sprite.x = msg.x;
+  sprite.y = msg.y;
+};
+
+Client.prototype.displayError = function(err) {
+  console.log('Websocketerror: ' + err);
+};
